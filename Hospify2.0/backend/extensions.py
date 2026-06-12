b88ce529389e59ae72a33385dd973a951d0f3622
Adeel <122879387+stranger1rajpoot@users.noreply.github.com>
2026-06-12 11:20:07 -0700

    Delete Hospify2.0 directory

--- Hospify2.0/backend/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
-from flask_cors import CORS
-from flask_mail import Mail
-
-db = SQLAlchemy()
-jwt = JWTManager()
-cors = CORS()
-mail = Mail()
--- Hospify2.0/backend/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from routes.auth import auth_bp
-from routes.patients import patients_bp
-from routes.appointments import appointments_bp
-from routes.pharmacy import pharmacy_bp, billing_bp
-from routes.lab import lab_bp, prescriptions_bp
-from routes.admin import admin_bp
-from routes.medicines import medicines_bp
-
-__all__ = ['auth_bp', 'patients_bp', 'appointments_bp', 'pharmacy_bp', 'billing_bp', 'lab_bp', 'prescriptions_bp', 'admin_bp', 'medicines_bp']
